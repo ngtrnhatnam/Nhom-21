1d2653dfde0b28012cc0d64830a63a64285dd141
Nhom-21 <nhatnam.ngtr@gmail.com>
2025-03-12 15:24:53 +0700

    Fix

---
+++ script.js
@@ -0,0 +1,10 @@
+function loadPage(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => {
+            document.getElementById('content').innerHTML = "<p>Lỗi khi tải trang!</p>";
+        });
+}
